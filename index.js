4417a15138926f5a774ea681cec919f1a761e706
parashuramb <38427048+parashuramb@users.noreply.github.com>
2021-04-29 19:34:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,84 @@
-const express = require('express');
-const app = express();
-const http = require('http'); 
-var cors = require('cors')
-app.use(cors())
+// const express = require('express');
+// const app = express();
+// var cors = require('cors')
+// app.use(cors())
+// const server = http.createServer(app);
+// const io = require('socket.io')(server, {
+//     cors: {
+//         origin: "*",
+//         credentials: true
+//     }
+// });
+
+// io.on('connection', onConnection);
+
+// function onConnection(socket) {
+//     socket.on('drawing', (data) =>
+//         socket.broadcast.emit('drawing', data)
+//     );
+
+// socket.on('join', function (data) {
+
+//     console.log('======Joined Room========== ');
+//     console.log(data);
+
+//     // Json Parse String To Access Child Elements
+//     var messageJson = JSON.parse(data);
+//     const room = messageJson.room;
+//     console.log(room);
+
+//     socket.join(room);
+
+// });
+
+// // On Receiving Individual Chat Message (ic_message)
+// socket.on('ic_message', function (data) {
+//     console.log('======IC Message========== ');
+//     console.log(data);
+
+//     // Json Parse String To Access Child Elements
+//     // var messageJson = JSON.parse(data);
+//     var messageJson = data;
+//     const room = messageJson.room;
+//     const message = messageJson.message;
+
+//     console.log(room);
+//     // console.log(message);
+
+//     // Sending to all clients in room except sender
+//     socket.broadcast.to(room).emit('new_msg', message);
+
+// });
+
+// socket.on('disconnect', function () {
+//     console.log('one user disconnected ' + socket.id);
+// });
+
+// }
+
+// const port = 8080;
+// server.listen(port, () => console.log(`server is running on port ${port}`));
+
+
+const express = require("express");
+const cors = require("cors");
+const http = require('http');
+
+// create express app
+const app = express(); 
+app.use(cors());
+/* Request Middleware */
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Headers", "token");
+    res.header("Access-Control-Max-Age", "10000");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+app.set("view engine", "jade");
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
     cors: {
@@ -10,12 +86,13 @@ const io = require('socket.io')(server, {
         credentials: true
     }
 });
-
 io.on('connection', onConnection);
 
 function onConnection(socket) {
-    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+    socket.on('drawing', (data) =>
+        socket.broadcast.emit('drawing', data)
+    );
 }
 
-const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`server is running on port ${port}`));
+
+server.listen(process.env.PORT || 5000, () => console.log(`server is running on port ${ process.env.PORT || 5000}`));
