088e1a1258c58078156ecde4679745e217829ea7
parashur <parashuram.b@comorin.co>
2021-04-29 16:56:18 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http'); 
+var cors = require('cors')
+app.use(cors())
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        credentials: true
+    }
+});
+
+io.on('connection', onConnection);
+
+function onConnection(socket) {
+    socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+}
+
+const port = 8080;
+server.listen(port, () => console.log(`server is running on port ${port}`));
